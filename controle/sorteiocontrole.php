d365a0cc010903e0dca84f5ac9a94b094c84ec7d
epcgrs <emmanuelf988@gmail.com>
2018-04-17 16:54:35 -0300

    styles

--- controle/sorteiocontrole.php
+++ controle/sorteiocontrole.php
@@ -16,7 +16,7 @@ if ($s->qtd > 100 || $s->qtd <= 0) {
 	$html = "";
 
 	for($i=0 ; $i < $s->qtd ; $i++){
-		$html .= '<p>Dado '. ($i+1) . ' = <span>'. $s->verificar().'</span></p>';
+		$html .= '<p>Dado '. ($i+1) . ' = <span class="dice-number">'. $s->verificar().'</span></p>';
 	}
 
 	json_encode($html);
